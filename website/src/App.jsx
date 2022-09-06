9f1f21b814587d10fdb0e0cd3581f7c67fbee46f
Ian Cook <ianscook19@gmail.com>
2022-09-06 16:48:08 -0400

    tracking different page views

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -3,8 +3,16 @@ import { Route, HashRouter } from 'react-router-dom';
 import './App.css';
 import Blog from './pages/Blog';
 import Home from './pages/Home';
+import ReactGA from 'react-ga';
+const TRACKING_ID = "G-N5G4KBHM6D";
+ReactGA.initialize(TRACKING_ID);
 
 const App = () => {
+
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <div className='container'>
       <HashRouter>
