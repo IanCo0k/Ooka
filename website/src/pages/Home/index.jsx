f40b22f51ae19ab7bed26d08f90c9544baa5ba02
Ian Cook <ianscook19@gmail.com>
2022-09-06 20:21:13 -0400

    navbar

--- website/src/pages/Home/index.jsx
+++ website/src/pages/Home/index.jsx
@@ -2,25 +2,19 @@ import React, { useState } from 'react';
 import EmptyList from '../../components/common/EmptyList';
 import BlogList from '../../components/Home/BlogList';
 import Header from '../../components/Home/Header';
-import SearchBar from '../../components/Home/SearchBar';
 import { blogList } from '../../config/data';
 import { Helmet } from 'react-helmet'
+import styles from './styles.css';
 
 const Home = () => {
   const [blogs, setBlogs] = useState(blogList[0].id > blogList[blogList.length-1].id ? blogList : blogList.reverse());
-  const [searchKey, setSearchKey] = useState('');
 
-  // Search submit
-  const handleSearchBar = (e) => {
-    e.preventDefault();
-    handleSearchResults();
-  };
 
   // Search for blog by category
-  const handleSearchResults = () => {
+  const handleSearchResults = (searchResult) => {
 
     const filteredBlogs = blogList.filter((blog) =>
-      blog.category.toLowerCase().includes(searchKey.toLowerCase().trim())
+      blog.category.toLowerCase().includes(searchResult.toLowerCase().trim())
     );
     setBlogs(filteredBlogs);
   };
@@ -28,7 +22,7 @@ const Home = () => {
   // Clear search and show all blogs
   const handleClearSearch = () => {
     setBlogs(blogList);
-    setSearchKey('');
+    handleSearchResults('');
   };
 
   return (
@@ -41,15 +35,12 @@ const Home = () => {
 
         <div>
           <Header />
-          
+          <div className='navbar'>
+            <p onClick={() => handleSearchResults('NBA')}>NBA</p>
+            <p onClick={() => handleSearchResults('NCAA')}>NCAA</p>
+            <p onClick={() => handleClearSearch()}>ALL</p>
+          </div>
         </div>
-
-        <SearchBar
-          value={searchKey}
-          clearSearch={handleClearSearch}
-          formSubmit={handleSearchBar}
-          handleSearchKey={(e) => setSearchKey(e.target.value)}
-        />
 
       {/* Blog List & Empty View */}
       {!blogs.length ? <EmptyList /> : <BlogList blogs={blogs} />}
